2e55d4f5aea875363abff52774b30e8bc75abcd6
Mark <sipizork@gmail.com>
2020-04-26 14:50:28 +0200

    hello

---
+++ precache-manifest.1896a5bd9e0d31135242927810843ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82a475c4c0f6ec3685afcc580d7d9ce",
+    "url": "/monster-web/index.html"
+  },
+  {
+    "revision": "2f13083e3ff66e16c804",
+    "url": "/monster-web/static/css/main.1d481c10.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-web/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-web/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f13083e3ff66e16c804",
+    "url": "/monster-web/static/js/main.c8c3b477.chunk.js"
+  },
+  {
+    "revision": "2838228f7972b36ec831",
+    "url": "/monster-web/static/js/runtime-main.c002fa13.js"
+  }
+]);
